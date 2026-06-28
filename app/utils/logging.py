dc4392a4c9f8d3bd39808aafd50cdbccb2ca8622
paowuor <owuorpaul500@gmail.com>
2026-06-28 23:07:18 +0300

    Add logging utility with configurable logger instance

---
+++ app/utils/logging.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+from app.utils.config import Config
+
+def get_logger(name: str) -> logging.Logger:
+    """Get a configured logger instance."""
+    logger = logging.getLogger(name)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(getattr(logging, Config.LOG_LEVEL, logging.INFO))
+    
+    return logger
